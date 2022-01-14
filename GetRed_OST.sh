e31b2d8ffb07ea69760940b68a65d4a414b8f9d9
chasecrum <chase.crum@suse.com>
2022-01-14 09:26:19 -0600

    add commands to install rebol/view

--- GetRed_OST.sh
+++ GetRed_OST.sh
@@ -16,7 +16,7 @@ zypper in -y patterns-base-32bit
 # install dependencies
 zypper in -y libgdk_pixbuf-2_0-0 libgdk_pixbuf-2_0-0-32bit
 zypper in -y libgtk-3-0 libgtk-3-0-32bit
-zypper in -y glibc libcurl4 libcurl4-32bit 
+zypper in -y glibc libcurl4 libcurl4-32bit
 
 ### download and install Red Language binary
 mkdir $HOME/Red
@@ -25,8 +25,29 @@ cd $HOME/Red
 zypper in -y wget
 wget https://static.red-lang.org/dl/linux/red-064
 chmod +x red-064
+
+### must install rebol/view for GUI applications or get vew has no value error
+
+# install dep libxt and libXaw (and their 32 bit versions)
+zypper in -y libXt6 libXt6-32bit libXaw-devel libXaw-devel-32bit
+
+#download the tar file
+wget http://www.rebol.com/downloads/v278/rebol-view-278-4-3.tar.gz
+
+# now to unpack it...
+tar -xvf rebol-view-278-4-3.tar.gz
+
+# let's go get it...
+cd releases/rebol-view
+
+# install the binary:
+./rebol
+
+
+
+
+
 echo "The Red language (and its dependencies) has been installed"
 echo "Please go to /root/Red and execute the cimpiler binary"
 echo "by using the command (as root): ./red-064"
 echo "YOUR RED LANGUAGE PROGRAMS WILL NOT WORK UNTIL YOU DO"
-
